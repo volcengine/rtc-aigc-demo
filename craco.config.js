6d0a023ca4c5ea4a3e02a4137a005670c7fa71f0
markshawn2020 <shawninjuly@gmail.com>
2025-06-03 13:52:05 +0800

    better package.json config

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,6 @@
  * SPDX-license-identifier: BSD-3-Clause
  */
 
-const CracoLessPlugin = require('craco-less');
 const path = require('path');
 
 module.exports = {
@@ -11,17 +10,58 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-  },
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-          },
-        },
-      },
+    configure: (webpackConfig) => {
+      // 添加 Less 支持
+      const oneOfRule = webpackConfig.module.rules.find((rule) => rule.oneOf);
+      if (oneOfRule) {
+        const lessRule = {
+          test: /\.less$/,
+          exclude: /\.module\.less$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'less-loader',
+              options: {
+                lessOptions: {
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ],
+        };
+
+        const lessModuleRule = {
+          test: /\.module\.less$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[local]--[hash:base64:5]',
+                },
+              },
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                lessOptions: {
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ],
+        };
+
+        // 在 file-loader 之前插入 Less 规则
+        const fileLoaderIndex = oneOfRule.oneOf.findIndex(
+          (rule) => rule.loader && rule.loader.includes('file-loader')
+        );
+        oneOfRule.oneOf.splice(fileLoaderIndex, 0, lessRule, lessModuleRule);
+      }
+
+      return webpackConfig;
     },
-  ],
+  },
 };
