f8d0bcc10fd5c5be75f0dd974ce88f568675163c
markshawn2020 <shawninjuly@gmail.com>
2025-06-04 02:09:54 +0800

    fix disconnect room before leave

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -9,8 +9,24 @@ import Sidebar from '@/components/Sidebar';
 import utils from '@/utils/utils';
 import MainArea from './MainArea';
 
+import { useEffect } from 'react';
+import { useLeave } from '@/lib/useCommon';
+
 export default function () {
   const isMobile = utils.isMobile();
+
+  // 页面刷新/关闭前自动断开 socket
+  const leave = useLeave();
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      // leave 返回 Promise，不阻塞刷新，但可确保断开 socket
+      leave();
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [leave]);
 
   return (
     <ResizeWrapper>
